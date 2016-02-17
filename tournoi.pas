16e7728ecee5bc8194f0d6affcf3ae4a440baf74
David <david.rigaux@eisti.fr>
2016-02-17 18:00:16 +0100

    Ajout modification, à ajouter dans git master et ajoute fichiers sur mac

--- tournoi.pas
+++ tournoi.pas
@@ -4,11 +4,11 @@ INTERFACE
 
 USES
 
-TYPE tabdyn1 = ARRAY OF STRING;
-     tabdyn2 = ARRAY OF INTEGER;
+TYPE tabdyni = ARRAY OF STRING;
+     tabdyns = ARRAY OF INTEGER;
 
-VAR t1 : tabdyn1;
-    t2 : tabdyn2;
+VAR t1 : tabdyns;
+    t2 : tabdyni;
 
 FUNCTION result(VAR t1:tabdyn1; t2:tabdyn2);
     var i, x : integer;
@@ -21,3 +21,33 @@ FUNCTION result(VAR t1:tabdyn1; t2:tabdyn2);
               else writeln('Match nul');
 
         end;
+
+PROCEDURE wel_tournoi;
+BEGIN
+	WRITELN('Bienvenue');
+	WRITE('Veuillez entrer le nombre d''equipes dans la compétition : ');
+END;
+FUNCTION creanoms(x : INTEGER) : tabdyns;
+VAR
+	i : INTEGER
+	entrenom : STRING;
+BEGIN
+	length(noms,x);
+	WRITELN('Veuillez entrer le noms de chaque equipes :');
+	FOR i := 0 TO x-1 DO BEGIN
+		READLN(entrenom);
+		noms[i] := entrenom;
+	END;
+	creanoms := noms;
+END;
+
+PROCEDURE tournois;
+VAR
+	x,i : INTEGER;
+	noms : tabdyns;
+BEGIN
+	wel_tournois;
+	readln(x);
+	noms := creanoms(x);
+	FOR i := 0 TO x-1 DO WRITELN(noms[i]);
+END;
